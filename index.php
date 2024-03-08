829c8562b8a315ad35a5e6e7a2e4bf5c35b0f378
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-08 13:16:56 +0100

    feat: add include_dir for items and items.php code

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include_once __DIR__ . '/models/product.php'
+include_once __DIR__ . '/models/items.php'
 ?>
 
 
---
+++ models/items.php
@@ -0,0 +1,11 @@
+<?php
+
+class Items {
+    public $type;
+
+    public function __construct($_type) {
+        $this->type = $_type;
+    }
+}
+
+?>
