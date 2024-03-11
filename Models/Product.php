9d6fd43af6be1d40653d3065a5b7f9cba8d1ea94
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-11 13:31:47 +0100

    fix: add trait with discount

--- Models/Product.php
+++ Models/Product.php
@@ -1,27 +1,50 @@
 <?php
 
-    class Product {
-        public $title;
-        public $icon;
-        public $price;
-        public $img;
-        public $category;
+// Definizione del trait Discount
+trait Discount {
+    protected $discountPercentage;
 
-        public function __construct($_title, $_icon, $_price, $_img, $_category) {
-            $this->title = $_title;
-            $this->icon = $_icon;
-            $this->price = $_price;
-            $this->img = $_img;
-            $this->category = $_category;
-        }
-
-        public function getProductInfo(): string{
-            return $this->title . ' , ' . $this->icon. ' , ' . $this->category->name;
-        }
-
-
-        public function getPrice() {
-            return $this->price;
-        }
+    public function setDiscountPercentage($percentage) {
+        $this->discountPercentage = $percentage;
     }
+
+    public function applyDiscount() {
+        if ($this->discountPercentage) {
+            $discountedPrice = $this->price - ($this->price * $this->discountPercentage / 100);
+            return $discountedPrice;
+        }
+        return $this->price;
+    }
+}
+
+// Definizione della classe Product con l'utilizzo del trait Discount
+class Product {
+    use Discount;
+
+    public $title;
+    public $icon;
+    public $price;
+    public $img;
+    public $category;
+
+    public function __construct($_title, $_icon, $_price, $_img, $_category) {
+        $this->title = $_title;
+        $this->icon = $_icon;
+        $this->price = $_price;
+        $this->img = $_img;
+        $this->category = $_category;
+    }
+
+    public function getProductInfo(): string {
+        return $this->title . ', ' . $this->icon . ', ' . $this->category->name;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function getDiscountedPrice() {
+        return $this->applyDiscount();
+    }
+}
 ?>
